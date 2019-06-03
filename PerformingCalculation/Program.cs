62ff698d5c0bd16ab704c5f338e8289514b24295
auyeskhanm <auyeskhanm@gmail.com>
2019-06-03 21:18:41 +0600

    Добавьте файлы проекта.

---
+++ PerformingCalculation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PerformingCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("It's a mistake. Try once more;)");
+            }
+            else
+            {
+                var firsNumber = int.Parse(args[0]);
+                var secondNumber = int.Parse(args[1]);
+
+                Console.WriteLine($"Amount from calculator application = {firsNumber + secondNumber}");
+            }
+        }
+    }
+}
